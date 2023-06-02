b5ca3373c542a9bb4a4a27bec1b56276a62f80e4
Adithi13 <113155833+Adithi13@users.noreply.github.com>
2023-06-02 20:53:09 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,9 +1,9 @@
 i = 2
 d = 10.0
 s = 'Hello '
-a = int(input())
-b = float(input())
-c = input()
+a = int(input()) #if user input then int(input("enter a number"))
+b = float(input())#if user input then int(input("enter a number"))
+c = input()#if user input then input("enter a character")
 print(i + a)
 print(d + b)
 print(s + c)
